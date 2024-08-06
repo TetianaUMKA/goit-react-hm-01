a50973d6af85fb6688f16d40bc4f5448358e9c63
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-06 22:28:02 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,18 @@
+import userData from "../../userData.json";
+import Profile from "../Profile/Profile";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
 }
 
 export default App;
