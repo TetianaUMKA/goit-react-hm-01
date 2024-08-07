f0dc4facbbf500750e8caa3b54e2fee5d855d020
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-08-07 13:46:56 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,10 @@
 import userData from "../../userData.json";
 import friends from "../../friends.json";
+import transactions from "../../transactions.json";
 
 import Profile from "../Profile/Profile";
 import FriendList from "../FriendList/FriendList";
+import Transactions from "../TransactionHistory/Transactions";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <Transactions items={transactions} />
     </>
   );
 }
